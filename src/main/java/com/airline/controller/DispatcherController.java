3323bf40ffcd031122ea17c6010331c9064972ff
without eyes <37347347+without-eyes@users.noreply.github.com>
2025-03-23 18:45:08 +0200

    Add controllers

---
+++ src/main/java/com/airline/controller/DispatcherController.java
@@ -0,0 +1,38 @@
+package com.airline.controller;
+
+import com.airline.model.CrewMember;
+import com.airline.model.Flight;
+import com.airline.service.CrewMemberService;
+import com.airline.service.FlightService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/dispatcher/crew")
+public class DispatcherController {
+    private final CrewMemberService crewMemberService;
+    private final FlightService flightService;
+
+    public DispatcherController(CrewMemberService crewMemberService, FlightService flightService) {
+        this.crewMemberService = crewMemberService;
+        this.flightService = flightService;
+    }
+
+    @GetMapping("/{flightId}")
+    public List<CrewMember> getCrewByFlight(@PathVariable Long flightId) {
+        return crewMemberService.getCrewByFlight(flightId);
+    }
+
+    @PostMapping("/{flightId}")
+    public CrewMember addCrewMember(@PathVariable Long flightId, @RequestBody CrewMember crewMember) {
+        Flight flight = flightService.getFlightById(flightId);
+        crewMember.setFlight(flight);
+        return crewMemberService.saveCrewMember(crewMember);
+    }
+
+    @DeleteMapping("/{id}")
+    public void removeCrewMember(@PathVariable Long id) {
+        crewMemberService.deleteCrewMember(id);
+    }
+}
